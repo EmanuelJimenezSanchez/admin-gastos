9d12130df164279bc577998dd94e0fe8daa44c28
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-01-25 21:12:34 -0600

    Creacion del modal para añadir nuevos gastos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script setup>
-  import { ref } from "vue"
+  import { ref, reactive } from "vue"
   import Presupuesto from "./components/Presupuesto.vue"
   import ControlPresupuesto from "./components/ControlPresupuesto.vue";
+  import Modal from "./components/Modal.vue"
 
   import iconoNuevoGasto from "./assets/img/nuevo-gasto.svg"
 
+  const modal = reactive({
+    mostrar: false,
+    animar: false
+  })
   const presupuesto = ref(0)
   const disponible = ref(0)
 
   const definirPresupuesto = (cantidad) => {
     presupuesto.value = cantidad
     disponible.value = cantidad
+  }
+
+  const mostrarModal = () => {
+    modal.mostrar = true
+    setTimeout(() => {
+      modal.animar = true
+    }, 300)
+  }
+
+  const ocultarModal = () => {
+    modal.animar = false
+    setTimeout(() => {
+      modal.mostrar = false
+    }, 300)
   }
 </script>
 
@@ -40,8 +59,15 @@
         <img 
           :src="iconoNuevoGasto"
           alt="Icono Nuevo Gasto"
+          @click="mostrarModal"
         />
       </div>
+
+      <Modal 
+        v-if="modal.mostrar"
+        @ocultar-modal="ocultarModal"
+        :modal="modal"
+      />
     </main>
   </div>
 </template>
